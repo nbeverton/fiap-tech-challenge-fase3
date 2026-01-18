8e0ce674ffb1eecd03aab7bb9490a1db9e4879aa
Estevao <estevoliveira1@gmail.com>
2026-01-18 15:46:02 -0300

    created final report

--- src/main/java/br/com/fiap/techchallenge/infra/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/techchallenge/infra/config/SwaggerConfig.java
@@ -1,16 +1,24 @@
 package br.com.fiap.techchallenge.infra.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+@OpenAPIDefinition
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "API JavaEats",
-                version = "v1",
-                description = "API responsável pelo gerenciamento de restaurantes"
-        )
-)
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI dsmovieAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("API JavaEats")
+                        .description("API responsável pelo gerenciamento de restaurantes")
+                        .version("v1.0.0")
+                        .license(new License()
+                                .url("https://github.com/nbeverton/fiap-tech-challenge-fase2")));
+    }
 }
