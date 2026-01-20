964c3116b8a536ca427974aaca0010a5db403b70
novaes1998 <lucas.novaes1109@gmail.com>
2026-01-20 14:11:13 -0300

    Adjusted the CreateUserAddress use case to correctly handle the business rule for principal addresses.
    When a new UserAddress is created with principal = true, the existing
    principal address (if any) is now automatically updated to principal = false,
    ensuring that a user has only one primary address at a time.
    
    This change enforces consistency in the user-address relationship and aligns
    the behavior with existing business rules already applied in other flows.

--- src/main/java/br/com/fiap/techchallenge/core/usecase/impl/useraddress/CreateUserAddressUseCaseImpl.java
+++ src/main/java/br/com/fiap/techchallenge/core/usecase/impl/useraddress/CreateUserAddressUseCaseImpl.java
@@ -22,9 +22,24 @@ public class CreateUserAddressUseCaseImpl implements CreateUserAddressUseCase {
     @Override
     public UserAddress execute(UserAddress userAddress) {
 
+        //1. Validate if the user exists
         findUserByIdUseCase.execute(userAddress.getUserId());
 
+        //2. Validate if the address exists
         findAddressByIdUseCase.execute(userAddress.getAddressId());
+
+        // 3. Validate if the new link will be the principal one; if so, set the old principal to false
+        boolean isPrincipal = userAddress.isPrincipal();
+
+        //4. Search for the current primary address (if it exists)
+        if(isPrincipal){
+            userAddressRepository.findPrincipalByUserId(userAddress.getUserId())
+                    .ifPresent(existingPrincipal -> {
+                        existingPrincipal.setPrincipal(false);
+
+                        userAddressRepository.save(existingPrincipal);
+                    });
+        }
 
         return userAddressRepository.save(userAddress);
     }
