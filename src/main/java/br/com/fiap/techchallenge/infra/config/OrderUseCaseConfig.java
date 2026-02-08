e0489579536ec44c2fa278ec43bc94b87603f91b
Luís Fernando Nascimento <luisfernandonascimentopro@gmail.com>
2026-02-08 16:11:08 -0300

    chore: configure OrderUseCases and prepare for Spring Boot integration

--- src/main/java/br/com/fiap/techchallenge/infra/config/OrderUseCaseConfig.java
+++ src/main/java/br/com/fiap/techchallenge/infra/config/OrderUseCaseConfig.java
@@ -6,23 +6,26 @@ import br.com.fiap.techchallenge.core.usecase.in.order.AcceptOrderUseCase;
 import br.com.fiap.techchallenge.core.usecase.in.order.CreateOrderUseCase;
 import br.com.fiap.techchallenge.core.usecase.impl.order.AcceptOrderUseCaseImpl;
 import br.com.fiap.techchallenge.core.usecase.impl.order.CreateOrderUseCaseImpl;
+import br.com.fiap.techchallenge.core.usecase.impl.order.OrderManagementUseCase;
 import br.com.fiap.techchallenge.core.usecase.out.OrderRepositoryPort;
-
 
 @Configuration
 public class OrderUseCaseConfig {
 
     @Bean
+    public OrderManagementUseCase orderManagementUseCase(OrderRepositoryPort orderRepositoryPort) {
+        return new OrderManagementUseCase(orderRepositoryPort);
+    }
+
+    @Bean
     public CreateOrderUseCase createOrderUseCase(
-            OrderRepositoryPort orderRepositoryPort
-    ) {
+            OrderRepositoryPort orderRepositoryPort) {
         return new CreateOrderUseCaseImpl(orderRepositoryPort);
     }
 
     @Bean
     public AcceptOrderUseCase acceptOrderUseCase(
-            OrderRepositoryPort orderRepositoryPort
-    ) {
+            OrderRepositoryPort orderRepositoryPort) {
         return new AcceptOrderUseCaseImpl(orderRepositoryPort);
     }
 }
