1114a9cdbd1bec337c137eb57f2fbdf3e74395ac
novaes1998 <lucas.novaes1109@gmail.com>
2026-02-18 22:21:57 -0300

    refactor(core): adjust return type in PaymentRepositoryPort

--- src/main/java/br/com/fiap/techchallenge/core/usecase/out/PaymentRepositoryPort.java
+++ src/main/java/br/com/fiap/techchallenge/core/usecase/out/PaymentRepositoryPort.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface PaymentRepositoryPort {
 
-    void save(Payment payment);
+    Payment save(Payment payment);
 
     Optional<Payment> findById(String paymentId);
 
