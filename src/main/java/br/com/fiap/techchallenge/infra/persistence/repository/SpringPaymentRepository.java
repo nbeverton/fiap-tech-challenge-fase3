d6130ea33c2069b81a8143b31124d774235ad237
novaes1998 <lucas.novaes1109@gmail.com>
2026-02-18 21:34:22 -0300

    feat(infra): add PaymentMongoRepository for persistence operations

---
+++ src/main/java/br/com/fiap/techchallenge/infra/persistence/repository/SpringPaymentRepository.java
@@ -0,0 +1,11 @@
+package br.com.fiap.techchallenge.infra.persistence.repository;
+
+import br.com.fiap.techchallenge.infra.persistence.documents.PaymentDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface SpringPaymentRepository extends MongoRepository<PaymentDocument, String> {
+
+    List<PaymentDocument> findByOrderId(String orderId);
+}
