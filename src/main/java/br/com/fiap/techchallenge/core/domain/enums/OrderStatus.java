5dc56f56ae0a12bf1aa97823e2d4301e3f21672e
novaes1998 <lucas.novaes1109@gmail.com>
2026-02-20 17:37:46 -0300

    feat(order): introduce PAYMENT_CONFIRMED intermediate status
    - add PAYMENT_CONFIRMED to OrderStatus
    - support payment confirmation before order acceptance

--- src/main/java/br/com/fiap/techchallenge/core/domain/enums/OrderStatus.java
+++ src/main/java/br/com/fiap/techchallenge/core/domain/enums/OrderStatus.java
@@ -3,6 +3,7 @@ package br.com.fiap.techchallenge.core.domain.enums;
 public enum OrderStatus {
     CREATED,
     AWAITING_PAYMENT,
+    PAYMENT_CONFIRMED,
     PAID,
     PREPARING,
     OUT_FOR_DELIVERY,
