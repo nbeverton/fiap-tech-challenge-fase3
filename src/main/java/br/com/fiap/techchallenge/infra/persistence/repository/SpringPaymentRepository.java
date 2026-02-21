df1fbd5094f48963a33e3f61428cc6bde4b8fec9
novaes1998 <lucas.novaes1109@gmail.com>
2026-02-21 00:38:49 -0300

    fix(payment): remove custom saveAll from SpringPaymentRepository

--- src/main/java/br/com/fiap/techchallenge/infra/persistence/repository/SpringPaymentRepository.java
+++ src/main/java/br/com/fiap/techchallenge/infra/persistence/repository/SpringPaymentRepository.java
@@ -8,6 +8,4 @@ import java.util.List;
 public interface SpringPaymentRepository extends MongoRepository<PaymentDocument, String> {
 
     List<PaymentDocument> findByOrderId(String orderId);
-
-    void saveAll(List<PaymentDocument> payments);
 }
